ff53350ae5317f80c66493a7953f8641288d2078
Luke Marsden <me@lukemarsden.net>
2023-07-14 17:12:06 +0100

    code to set random seed

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM nvidia/cuda:12.6.2-base-ubuntu20.04
 #
 # NB: diffusers downgrade is because of https://github.com/cloneofsimo/lora/issues/231
 RUN apt-get update -y && apt-get install -y python3 python3-pip git && \
-    pip install git+https://github.com/cloneofsimo/lora.git@v0.1.7 && \
+    pip install git+https://github.com/lukemarsden/lora.git@v0.1.7 && \
     pip install diffusers==0.14 && \
     pip install accelerate==0.20.3 && \
     python3 -c "from diffusers import StableDiffusionPipeline; model_id = 'runwayml/stable-diffusion-v1-5'; StableDiffusionPipeline.from_pretrained(model_id)"
--- lora_diffusion/__init__.py
+++ lora_diffusion/__init__.py
@@ -3,3 +3,20 @@ from .dataset import *
 from .utils import *
 from .preprocess_files import *
 from .lora_manager import *
+
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print(f"Random seed set as {seed}")
+
+if os.getenv("RANDOM_SEED") is not None:
+    set_seed(int(os.getenv("RANDOM_SEED")))
+else:
+    set_seed(42)
